fa912c8cd4bd5d6eabe4d2de50080d6b9eb43c2e
Sachin Shah <sachinshah1999@gmail.com>
2023-12-15 19:05:45 +0530

    datatypes done

---
+++ Day-1/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // this use the new version of JS
+
+console.log('hello JavaScript')
+
+// alert("hello") // it only work in browser engine
+
+let num = 123
+let name = "Hello, I am string."
+let isStudent = true
+let nullValue = null
+let age;
+
+// datatypes
+
+// number 
+// string => ""
+// bigint 
+// boolean => true/false 
+// null => standalone value 
+// undefined => undefined 
+// symbol => unique
+
+console.log(typeof(num))
+console.log(typeof(isStudent))
+console.log(typeof(age))
+console.log(typeof(name))
+
+console.log(typeof(null)) // object - some say javascript problem some say some stories
+console.log(typeof(undefined)) // undefiend
