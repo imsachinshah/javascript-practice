ee9a815e7bfcf9174221bb99805339950cc8fb8d
Sachin Shah <sachinshah1999@gmail.com>
2023-12-18 19:40:35 +0530

    day 2 done

---
+++ Basics/06_arrays.js
@@ -0,0 +1,33 @@
+// Array
+
+const arr = [1, 2, 3, 4, 5]
+const arr1 = new Array(1, 2, 3, 4, 7)
+
+const arn = [true, "hello", 1, 2, 3]
+
+// console.log(arr1)
+// console.log(typeof arr1)
+
+// console.log(arn)
+// console.log(arr.length)
+
+const p = [5, 6, 7, 9]
+
+// console.log(p)
+
+p.push(7)
+p.push(9)
+p.pop()
+
+// console.log(p)
+
+const a = [3, 4, 5, [2, 3, 5], [5, 6, [9, 8, 0], 0]]
+// console.log(a.flat(Infinity))
+
+const c = [2, 4, 5]
+const d = [8, 9, 0, 7]
+
+// c.push(d)
+console.log(c.includes(9))
+console.log(c.join()) // converts into string
+console.log(c)
