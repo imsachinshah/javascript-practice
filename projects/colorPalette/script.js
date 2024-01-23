ee1c038045e92fdaebf36946715869a3eeea1597
Sachin Shah <sachinshah1999@gmail.com>
2024-01-23 20:24:18 +0530

    done colorPaletteGenerator

--- projects/colorPalette/script.js
+++ projects/colorPalette/script.js
@@ -1,24 +1,56 @@
-const colors = document.querySelectorAll('.color');
-const button = document.querySelector('button');
+const colors = document.querySelectorAll(".color");
+const colourCodes = document.querySelectorAll(".colorCode");
+const button = document.querySelector("button");
 
-const paletteURL = 'http://www.colourlovers.com/api/palettes/random';
-const url = 'http://colormind.io/api/';
+const url = "http://colormind.io/api/";
+
 const xhr = new XMLHttpRequest();
 
 let data = {
-    model: 'default',
-    input: [[44,43,44],[90,83,82],"N","N","N"]
+  model: "default",
+  input: ["N", "N", "N", "N", "N"],
+};
+
+const componentToHex = (c) => {
+  const hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
-async function getPalette() {
-   xhr.open('POST', url, true);
-   xhr.onreadystatechange = () => {
-    if(xhr.readyState === 4){
-        console.log(xhr.responseText)
+const rgbToHex = (r, g, b) => {
+  return ("#" + componentToHex(r) + componentToHex(g) + componentToHex(b)).toUpperCase();
+}
+
+function changeColors(palette) {
+  colors.forEach((color, index) => {
+    color.style.backgroundColor = `rgb(${palette[index][0]}, ${palette[index][1]}, ${palette[index][2]})`;
+    color.firstElementChild.textContent = `${rgbToHex(palette[index][0], palette[index][1], palette[index][2])}`
+  });
+}
+
+function getPalette() {
+  let palette;
+  xhr.open("POST", url, true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      palette = JSON.parse(xhr.responseText).result;
+      changeColors(palette);
     }
-   }
-   xhr.send(JSON.stringify(data));
+  };
+  xhr.send(JSON.stringify(data));
 
 }
 
-button.addEventListener('click', getPalette);
+button.addEventListener("click", getPalette);
+
+// colourCodes.forEach((colourCode) => {
+//   console.log(colourCode)
+//   colourCode.addEventListener('click', async function textCopy(){
+//     let text = colourCode.textContent;
+//     try {
+//         await navigator.clipboard.writeText(text);
+//         alert(`${text} copied to clipboard!`);
+//     } catch (error) {
+//         console.log(error);
+//     }
+//   })
+// })
